ae959646d381493dbd95f7d27f8960a5d75837e5
mhaddi12 <muhammad.haddi@bitlogicx.com>
2024-08-27 15:46:40 +0500

    changes by haddi

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -6,14 +6,14 @@ import 'package:image_picker/image_picker.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:mix/mix.dart';
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+class OnBoardingView extends StatefulWidget {
+  const OnBoardingView({super.key});
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
+  State<OnBoardingView> createState() => _OnBoardingViewState();
 }
 
-class _RegisterViewState extends State<RegisterView> {
+class _OnBoardingViewState extends State<OnBoardingView> {
   TextEditingController nameController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
   File? file;
@@ -24,229 +24,96 @@ class _RegisterViewState extends State<RegisterView> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
-      resizeToAvoidBottomInset: true,
-      body: InkWell(
-        splashColor: Colors.transparent,
-        onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
+    return  Scaffold(
+      body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Box(
-                style: Style(
-                  $box.width(screenWidth),
-                  $box.height(screenHeight),
-                  $box.padding(32),
-                  $box.color(isDarkMode ? Colors.black : Colors.white),
-                  $box.elevation(0),
-                ),
-                child: VBox(
-                  style: Style(
-                    $flex.mainAxisAlignment.center(),
-                    $flex.crossAxisAlignment.center(),
-                    $flex.gap(20),
-                  ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   children: [
-                    StyledText(
-                      "Register",
-                      style: Style(
-                        $text.style.fontSize(24),
-                        $text.style.fontWeight.w600(),
-                        $text.style
-                            .color(isDarkMode ? Colors.white : Colors.black),
-                      ),
-                    ),
-                    PressableBox(
-                      onPress: () => getImage(),
-                      style: Style(
-                        $box.border.all(
-                          width: 2,
-                          color: isDarkMode
-                              ? Colors.grey.shade700
-                              : Colors.grey.shade300,
-                        ),
-                        $box.shape.circle(),
-                        $box.width(152),
-                        $box.height(152),
-                        $box.shadow(
-                          color: isDarkMode
-                              ? Colors.grey.shade900
-                              : Colors.grey.shade500,
-                          blurRadius: 4,
-                          offset: const Offset(0, 2),
-                        ),
-                        $box.color(isDarkMode ? Colors.black : Colors.white),
-                      ),
-                      child: Stack(
-                        children: [
-                          ClipOval(
-                            child: StyledImage(
-                              style: Style(
-                                $image.width(152),
-                                $image.height(152),
-                                $image.fit.cover(),
-                                $image.alignment.center(),
-                              ),
-                              image: file == null
-                                  ? const NetworkImage(
-                                      'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg',
-                                    )
-                                  : FileImage(file!),
-                            ),
-                          ),
-                          if (file == null)
-                            Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: CircleAvatar(
-                                radius: 20,
-                                backgroundColor: isDarkMode
-                                    ? Colors.grey.shade800
-                                    : Colors.white,
-                                child: Icon(
-                                  Icons.add_a_photo,
-                                  color: isDarkMode
-                                      ? Colors.white
-                                      : Colors.grey.shade700,
-                                  size: 20,
-                                ),
-                              ),
-                            ),
-                        ],
-                      ),
-                    ),
-                    TextFormField(
+                    SizedBox(height: 20),
+                    Text(
+                      'Chatbox',
                       style: TextStyle(
-                        color: isDarkMode ? Colors.white : Colors.grey[900],
-                      ),
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        hintStyle: TextStyle(
-                            color: isDarkMode
-                                ? Colors.grey[400]
-                                : Colors.grey[700]),
-                        hintText: "Name",
-                        enabled: true,
-                        labelStyle: TextStyle(
-                            color: isDarkMode
-                                ? Colors.grey[400]
-                                : Colors.grey[700]),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(
-                              color: isDarkMode
-                                  ? Colors.grey
-                                  : Colors.grey.shade400),
-                        ),
-                        filled: true,
-                        fillColor: isDarkMode
-                            ? Colors.grey.shade900
-                            : Colors.grey.shade100,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    HBox(
-                      style: Style($flex.gap(12)),
+                    SizedBox(height: 50),
+                    Text(
+                      'Connect\nfriends\neasily &\nquickly',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Our chat app is the perfect way to stay\nconnected with friends and family.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16, color: Colors.white70),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Expanded(
-                          child: InternationalPhoneNumberInput(
-                            textStyle: TextStyle(
-                              color:
-                                  isDarkMode ? Colors.white : Colors.grey[900],
-                            ),
-                            onInputChanged: (number) {
-                              print(number.phoneNumber);
-                            },
-                            selectorConfig: const SelectorConfig(
-                              selectorType: PhoneInputSelectorType.DROPDOWN,
-                            ),
-                            initialValue: PhoneNumber(isoCode: 'PK'),
-                            textFieldController: phoneController,
-                            inputDecoration: InputDecoration(
-                              hintText: "Phone Number",
-                              hintStyle: TextStyle(
-                                  color: isDarkMode
-                                      ? Colors.grey[400]
-                                      : Colors.grey[700]),
-                              labelStyle: TextStyle(
-                                  color: isDarkMode
-                                      ? Colors.white
-                                      : Colors.grey[900]),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide(
-                                    color: isDarkMode
-                                        ? Colors.grey
-                                        : Colors.grey.shade400),
-                              ),
-                              filled: true,
-                              fillColor: isDarkMode
-                                  ? Colors.grey.shade900
-                                  : Colors.grey.shade100,
-                            ),
-                            formatInput: true,
-                            keyboardType: TextInputType.number,
-                          ),
+                        IconButton(
+                          icon: Icon(Icons.facebook),
+                          onPressed: () {
+                            // Handle Facebook sign-in
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.g_translate),
+                          onPressed: () {
+                            // Handle Google sign-in
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.apple),
+                          onPressed: () {
+                            // Handle Apple sign-in
+                          },
                         ),
                       ],
                     ),
-                    PressableBox(
-                      onPress: () {
-                        if (nameController.text.isEmpty) {
-                          Get.snackbar(
-                            "Failed",
-                            "Name Field is Empty",
-                            backgroundColor:
-                                isDarkMode ? Colors.grey[900] : Colors.grey,
-                            colorText: Colors.red,
-                            snackPosition: SnackPosition.TOP,
-                            icon:  Icon(Icons.sms_failed_outlined,color: Colors.red,),
-                          );
-                          return;
-                        } else if (phoneController.text.isEmpty) {
-                          Get.snackbar(
-                            "Failed",
-                            "Number Field is Empty",
-                            backgroundColor:
-                                isDarkMode ? Colors.grey[900] : Colors.grey,
-                            colorText: Colors.red,
-                            snackPosition: SnackPosition.TOP,
-                          );
-                          return;
-                        } else {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => const RegisterEmail(),
-                            ),
-                          );
-                        }
+                    SizedBox(height: 10),
+                    Text('OR', style: TextStyle(color: Colors.white70)),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle Sign up with email
                       },
-                      style: Style(
-                        $box.width(150),
-                        $box.height(50),
-                        $box.color(
-                            Colors.orangeAccent),
-                        $box.borderRadius(25),
-                        $box.elevation(2),
-                        $text.style.color(Colors.white),
-                        $text.style.fontWeight.w600(),
-                      ),
-                      child: Center(
-                        child: StyledText(
-                          "Continue",
-                          style: Style(
-                            $text.style.fontSize(16),
-                            $text.style.fontWeight.w600(),
-                          ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
                         ),
+                      ),
+                      child: Text('Sign up with mail'),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        // Handle Log in
+                      },
+                      child: Text(
+                        'Existing account? Log in',
+                        style: TextStyle(color: Colors.white70),
                       ),
                     ),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
         ),
--- lib/views/splashScreen.dart
+++ lib/views/splashScreen.dart
@@ -2,7 +2,6 @@ import 'package:bits/views/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -11,30 +10,29 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-    @override
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(milliseconds: 2000),(){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>RegisterView()));
+    Future.delayed(Duration(milliseconds: 2000), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => OnBoardingView()));
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-        body: Center(
-          child: Box(
-            child: StyledImage(image: AssetImage('assets/images/Designer (2).png'),
+      body: Center(
+        child: Box(
+          child: StyledImage(
+            image: AssetImage('assets/images/Designer (2).png'),
             style: Style(
-              $image.fit.cover(),
-              $image.height(200),
-              $image.width(200)
-
-            ),
-            ),
+                $image.fit.cover(), $image.height(200), $image.width(200)),
           ),
         ),
+      ),
     );
   }
 }
